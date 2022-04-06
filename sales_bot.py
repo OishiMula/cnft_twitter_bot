096a81696e1cf3c625aa51eeaa316676379eba2b
Oishi Mula <thenefi@gmail.com>
2022-04-06 18:00:09 -0400

    first upload

---
+++ sales_bot.py
@@ -0,0 +1,12 @@
+# Twitter sales bot created by Oishi Mula for usage at Raging Teens Clan NFT.
+# Copyright (c) 2022, Oishi Mula
+# All rights reserved.
+# This source code is licensed under the MIT-style license found in the
+# LICENSE file in the root directory of this source tree. 
+import requests
+
+def main():
+    print("Hello World!")
+
+if __name__ == "__main__":
+    main()
