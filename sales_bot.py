09b94fe1078372fb11e40e271854a3852608d540
OishiMula <oishimula@gmail.com>
2022-04-08 20:37:52 -0400

    validation & catch-up added

--- sales_bot.py
+++ sales_bot.py
@@ -16,23 +16,24 @@ from ratelimit import limits
 
 load_dotenv()
 
-# Creating the Twitter tweepy connection for V2
-twitter = tweepy.Client(
-    bearer_token=os.getenv('bearer_token'),
+# Creating the Twitter tweepy connection for V1.1 (media_upload)
+twitter_v1_1_auth = tweepy.OAuth1UserHandler(
     consumer_key=os.getenv('consumer_key'),
     consumer_secret=os.getenv('consumer_secret'),
     access_token=os.getenv('access_token'),
     access_token_secret=os.getenv('access_token_secret')
 )
 
+twitter = tweepy.API(twitter_v1_1_auth)
+
 # OpenCNFT endpoint for sales on selected Policy ID
 rtc_sales_endpoint = f"https://api.opencnft.io/1/policy/{os.getenv('rtc_policyid')}/transactions?order=date"
 furin_sales_endpoint = f"https://api.opencnft.io/1/policy/{os.getenv('furin_policyid')}/transactions?order=date"
-ipfs_base = 'https://infura-ipfs.io/ipfs/'
 
 # Creating a first run to see if the file to store last transaciton is made
 first_run = True
 last_sold_file = Path('last_sold.dat')
+last_sold_furin_file = Path('last_furin_sold.dat')
 
 running = True
 MINUTE = 60
@@ -48,61 +49,94 @@ def millify(n):
 
 # The function to retrieve the JSON listings
 def retrieve_sales(p):
-    if p == 'rtc':
-        response_API = requests.get(f'{rtc_sales_endpoint}')
-    elif p == 'furin':
-        response_API = requests.get(f'{furin_sales_endpoint}')
-    return response_API.json()
+    match p:
+        case 'rtc':
+            response_API = requests.get(f'{rtc_sales_endpoint}')
+        case 'furin':
+            response_API = requests.get(f'{furin_sales_endpoint}')
+
+    if response_API.status_code == 200: return response_API.json()
+    else: return None
+
+def tweet_sale(listing):
+    # strings for easy tweeting
+    asset = listing['unit_name']
+    sold_price = int(float(listing['price']) / 1000000)
+    asset_mp = listing['marketplace']
+    asset_img_raw = listing['thumbnail']['thumbnail'][7:]
+    asset_media_id = retrieve_media_id(asset_img_raw)
+
+    twitter.update_status(status=f"{asset} was purchased from {asset_mp} for the price of {ada}{millify(sold_price)}.", media_ids=[asset_media_id.media_id])
+    os.remove('image.png')
+
+def retrieve_media_id(img_raw):
+    ipfs_base = 'https://infura-ipfs.io/ipfs/'
+    asset_img = requests.get(f"{ipfs_base}{img_raw}")
+    if asset_img.status_code == 200:
+        with open("image.png", 'wb') as f:
+            f.write(asset_img.content)
+            return twitter.media_upload("image.png")
+    else:
+        return twitter.media_upload("404.jpg")
+
+def compare_listings(current_listing, saved_listing):
+    last_sold_saved = pickle.load(open(saved_listing, 'rb'))
+    
+    print(f"Comparing saved to recent: {current_listing['items'][0]['unit_name']} --- {last_sold_saved['unit_name']}")
+    check_flag = True
+    x = 0
+    while check_flag == True:
+        if int(last_sold_saved['sold_at']) < current_listing['items'][x]['sold_at']:
+            x += 1
+            new_current = current_listing['items'][x]
+            print(f"Current: {current_listing['items'][x]['unit_name']}")
+            print("Older listing is more recent, adding.")
+            time.sleep(3)
+        else:
+            if x == 0:
+                print(f"Current: {current_listing['items'][0]['unit_name']} - pass")
+            while x > 0:
+                print(f"you have {x} listings. - tweeting now")
+                tweet_sale(current_listing['items'][x])
+                x -= 1 
+                time.sleep(3)
+            check_flag = False
+    try:
+        saved_listing = new_current['sold_at']
+    except:
+        pass
+
+    if int(current_listing['items'][0]['sold_at']) > int(last_sold_saved['sold_at']):
+        pickle.dump(current_listing[0], open(saved_listing, 'wb'))
+        print("Ding - New sale. Tweeting now.")
+        tweet_sale(current_listing['items'][0])
 
 @limits(calls=30, period=MINUTE)
 def main():
     global first_run
     global running
-    global ipfs_base
+
+    # Upon starting, it will check for a last_sold csve. If none exist, it will enter the most recent sale to begin the monitor.
+    if first_run == True:
+        first_run = False
+        if last_sold_file.is_file() == False:
+            current_sales_rtc = retrieve_sales('rtc')
+            pickle.dump(current_sales_rtc['items'][0], open(last_sold_file, 'wb'))
+        if last_sold_furin_file.is_file() == False:
+            current_sales_furin = retrieve_sales('furin')
+            pickle.dump(current_sales_furin['items'][0], open(last_sold_furin_file, 'wb'))
+
     while running == True:
-        current_sales = retrieve_sales('rtc')['items']
+        # Check if listings were retrieved, if not, timeout in case OpenCNFT is down
+        current_sales_rtc = retrieve_sales('rtc')
+        current_sales_furin =retrieve_sales('furin')
+        if current_sales_rtc == None or current_sales_furin == None:
+            time.sleep(120)
+            continue
 
-        # Upon starting, it will check for a last_sold csve. If none exist, it will enter the most recent sale to begin the monitor.
-        if first_run == True:
-            first_run = False
-            if last_sold_file.is_file() == False:
-                pickle.dump(current_sales[0], open('last_sold.dat', 'wb'))
-            
-        current_time = current_sales[0]['sold_at']
-        last_sold_meta = pickle.load(open('last_sold.dat', 'rb'))
-
-        if int(current_time) > int(last_sold_meta['sold_at']):
-            # check to see if the listing after was not skipped
-            check_flag = True
-            x = 0
-            while check_flag == True:
-                if current_time < current_sales[x]['sold_at']:
-                    x += 1
-                    current_time = current_sales[x]['sold_at']
-                    print("Older listing is more recent")
-                else:
-                    check_flag = False
-            
-            # save data for persistent data
-            pickle.dump(current_sales[x], open('last_sold.dat', 'wb'))
-
-            # setup for the string formats
-            asset = current_sales[x]['unit_name']
-            sold_price = int(float(current_sales[x]['price']) / 1000000)
-            asset_mp = current_sales[x]['marketplace']
-            asset_img_raw = current_sales[x]['thumbnail']['thumbnail'][7:]
-
-            asset_img = requests.get(f"{ipfs_base}{asset_img_raw}")
-            if asset_img.status_code == 200:
-                with open("image.png", 'wb') as f:
-                    f.write(asset_img.content)
-
-            # Waiting for Twitter V2 API approval
-            #twitter.create_tweet(text="Hello World!")
-            #twitter.create_tweet(text=f"{asset} was purchased from {asset_mp} for the price of {ada}{millify(sold_price)}.}")
-            os.remove('image.png')
-        time.sleep(3) 
+        compare_listings(current_sales_rtc, last_sold_file)
+        compare_listings(current_sales_furin, last_sold_furin_file)
+        time.sleep(30)
         
-
 if __name__ == "__main__":
     main()
